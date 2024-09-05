c1373b6c20a24507d675135f3ed8e1f5cafdb0e3
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-09-05 14:26:20 +0300

    translated Turkish

--- front-end/src/pages/PlanetsAndMoons/Mars/MarsAtmosphereAndClimate.js
+++ front-end/src/pages/PlanetsAndMoons/Mars/MarsAtmosphereAndClimate.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Header from '../../../components/Header/Header'; // Header componentini içe aktar
-import Footer from '../../../components/Footer/Footer';
+import React from "react";
+import Header from "../../../components/Header/Header"; // Header componentini içe aktar
+import Footer from "../../../components/Footer/Footer";
 //import styles from './MarsAtmosphereAndClimate.module.css'; // Bu sayfaya özel CSS dosyası
 
 const MarsAtmosphereAndClimate = () => (
   <div>
-    <Header 
-      title="Mars Atmosphere and Climate" 
-      subtitle="Explore the atmosphere and climate of Mars through detailed data and visualizations."
+    <Header
+      title="Mars Atmosferi ve İklimi"
+      subtitle="Mars'ın atmosferi ve iklimini ayrıntılı veriler ve görselleştirmelerle keşfedin."
     />
     {/* Sayfanın geri kalan içeriği */}
     <Footer /> {/* Footer componentini ekle */}
-
   </div>
 );
 
