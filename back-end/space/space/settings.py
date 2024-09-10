8bcdfda5850d1063c340a6cb6a33e5df8b89a48d
nazli <nazlinuresmeray049@gmail.com>
2024-09-10 15:13:09 +0300

    nasa_api_key added

--- back-end/space/space/settings.py
+++ back-end/space/space/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NASA_API_KEY = 'gnYVD5JdV6ypXGNY7fqKVhEtlct0ms49tyhEmmsL'
