2ec0efd97cff1f9db76a0b1c0164c5722e6ee402
nazli <nazlinuresmeray049@gmail.com>
2024-09-14 15:49:43 +0300

    MarsAtmosphereAndClimate updated

--- front-end/src/pages/PlanetsAndMoons/Mars/MarsAtmosphereAndClimate.js
+++ front-end/src/pages/PlanetsAndMoons/Mars/MarsAtmosphereAndClimate.js
@@ -37,17 +37,17 @@ const MarsAtmosphereAndClimate = () => {
   };
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100">
       <Header
         title="Mars Atmosferi ve İklimi"
         subtitle="Mars'ın atmosferi ve iklimini ayrıntılı veriler ve görselleştirmelerle keşfedin."
       />
-      <div className="p-4 max-w-4xl mx-auto">
-        <div className="mb-4">
-          <label htmlFor="solSelect" className="block text-lg font-medium text-gray-700 mb-2">Sol Seçin:</label>
+      <div className="p-6 max-w-4xl mx-auto bg-white rounded-lg shadow-lg">
+        <div className="mb-6">
+          <label htmlFor="solSelect" className="block text-lg font-semibold text-gray-800 mb-2">Sol Seçin:</label>
           <select
             id="solSelect"
-            className="form-select block w-full mt-1"
+            className="form-select block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200 ease-in-out"
             value={selectedSol || ''}
             onChange={handleSolChange}
           >
@@ -57,17 +57,17 @@ const MarsAtmosphereAndClimate = () => {
           </select>
         </div>
 
-        <div className="mb-4">
-          <label className="block text-lg font-medium text-gray-700 mb-2">Grafik Türü Seçin:</label>
+        <div className="mb-6">
+          <label className="block text-lg font-semibold text-gray-800 mb-2">Grafik Türü Seçin:</label>
           <div className="flex space-x-4">
             <button
-              className={`py-2 px-4 rounded-lg ${chartType === 'line' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              className={`py-2 px-6 rounded-lg font-bold text-white transition-all duration-300 ease-in-out transform ${chartType === 'line' ? 'bg-gradient-to-r from-blue-500 to-blue-700 shadow-lg scale-105' : 'bg-gray-300 text-gray-700'} hover:scale-110 hover:shadow-xl focus:outline-none`}
               onClick={() => handleChartTypeChange('line')}
             >
               Çizgi Grafiği
             </button>
             <button
-              className={`py-2 px-4 rounded-lg ${chartType === 'doughnut' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              className={`py-2 px-6 rounded-lg font-bold text-white transition-all duration-300 ease-in-out transform ${chartType === 'doughnut' ? 'bg-gradient-to-r from-blue-500 to-blue-700 shadow-lg scale-105' : 'bg-gray-300 text-gray-700'} hover:scale-110 hover:shadow-xl focus:outline-none`}
               onClick={() => handleChartTypeChange('doughnut')}
             >
               Dairesel Grafik
