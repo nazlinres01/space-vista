a7d5b053f36d57df9fc5afaedd3f31534005798c
Medine Kaynak <medinekaynak2906@gmail.com>
2024-10-16 02:21:58 +0300

    tüm veriler baştan yazılmaya başlandı-1.marsWeather

---
+++ back-end/marsWeather/marsWeather.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+#data visualize
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid")
+
+df=pd.read_csv("mars-weather.csv",index_col=0)
+df.head()
